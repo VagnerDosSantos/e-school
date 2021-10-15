a65e2207291f1aff19d760debd3e8d139f9e2d1c
VagnerDosSantos <vagner.guittar@gmail.com>
2021-10-15 20:40:10 -0300

    configuração inicial para testes automatizados

---
+++ tests/Student/StudentTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Student;
+
+use PHPUnit\Framework\TestCase;
+
+class StudentTest extends TestCase
+{
+    public function testInsertStudentInDatabase()
+    {
+        $this->assertTrue(true);
+    }
+}
